651c45d9e8cb4a589b692f94e40645c0e4e8d141
ashutoshtomar <astomar@thehive.ai>
2023-06-19 12:50:45 +0530

    see more with spannable

--- app/src/main/java/com/example/seemoreseeless/MsgAdapter.kt
+++ app/src/main/java/com/example/seemoreseeless/MsgAdapter.kt
@@ -1,10 +1,16 @@
 package com.example.seemoreseeless
 
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -27,25 +33,55 @@ class MsgAdapter(listdata: ArrayList<String>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val myListData: String = listdata[position]
 
-        holder.smallText.setText(listdata[position])
-        holder.longText.setText(listdata[position])
+        holder.smallText.maxLines =2
+        holder.smallText.text = "${position+1} ${listdata[position]}"
+        holder.longText.text = "${position+1} ${listdata[position]}"
 
-        holder.longText.post(Runnable {
-            var lineCount = holder.longText.lineCount
+        holder.longText.visibility = View.GONE
+        holder.smallText.visibility = View.VISIBLE
+        holder.smallText.post(Runnable {
+            var lineCount = holder.smallText.lineCount
             Log.d("abcd", "lines $lineCount")
+
             if (lineCount > 2) {
-                holder.smallText.maxLines = 2
-                holder.readMoreText.visibility = View.VISIBLE
-                holder.smallText.visibility = View.VISIBLE
-                holder.longText.visibility = View.GONE
-                holder.readMoreText.setOnClickListener {
-                    holder.smallText.visibility = View.GONE
-                    holder.longText.visibility = View.VISIBLE
-                    holder.readMoreText.visibility = View.GONE
+                val layout = holder.smallText.layout
+                val lineStart = layout.getLineStart(0);
+                val lineEnd = layout.getLineEnd(1);
+
+                val lineText = holder.smallText.text.substring(lineStart, lineEnd).toString();
+//
+                Log.d("abcd", "$lineText\n\n")
+
+
+                val textSpan = lineText.substring(lineStart, lineEnd - 13)
+                val finalString = "${textSpan}... See More"
+
+                Log.d("abcd","$finalString")
+                val sb: Spannable = SpannableString(finalString)
+
+                val clickableSpan: ClickableSpan = object : ClickableSpan() {
+                    override fun onClick(textView: View) {
+                        holder.smallText.visibility = View.GONE
+                        holder.longText.visibility = View.VISIBLE
+                    }
+
+                    override fun updateDrawState(ds: TextPaint) {
+                        super.updateDrawState(ds)
+                        ds.isUnderlineText = false
+                    }
                 }
+                sb.setSpan(
+                    clickableSpan,
+                    finalString.length - 8,
+                    finalString.length,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                holder.smallText.text = sb
+                holder.smallText.movementMethod = LinkMovementMethod.getInstance()
+                holder.smallText.highlightColor = Color.GREEN
+
             } else {
                 holder.longText.visibility = View.GONE
-                holder.readMoreText.visibility = View.GONE
                 holder.smallText.visibility = View.VISIBLE
 
             }
@@ -61,12 +97,10 @@ class MsgAdapter(listdata: ArrayList<String>) :
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var smallText: TextView
         var longText: TextView
-        var readMoreText: TextView
 
         init {
             smallText = itemView.findViewById<View>(R.id.small_text) as TextView
             longText = itemView.findViewById<View>(R.id.long_text) as TextView
-            readMoreText = itemView.findViewById<View>(R.id.see_more) as TextView
         }
     }
 }
